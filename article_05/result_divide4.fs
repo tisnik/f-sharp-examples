574a0f70af85f10e8deba4f7fddc6551f876acb1
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-10-09 07:36:06 +0200

    Result data type and pattern matching, without explicit parameters type

---
+++ article_05/result_divide4.fs
@@ -0,0 +1,12 @@
+let divide x y =
+    match y with
+    | 0 -> Error "divide by zero"
+    | _ -> Ok (x/y)
+
+let print_result value =
+    match value with
+    | Ok(x) -> printf "%d" x
+    | Error(x) -> printf "hmm, dělení nulou"
+
+print_result (divide 10 2)
+print_result (divide 1 0)
