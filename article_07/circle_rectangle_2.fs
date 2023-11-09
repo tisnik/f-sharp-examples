5966d1c7d8a8ab9147b6ddfdb5bf92da04d1d96d
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-09 09:40:33 +0100

    Circle or rectangle data type, example #2

---
+++ article_07/circle_rectangle_2.fs
@@ -0,0 +1,14 @@
+type Shape = 
+    | Circle of int
+    | Rectangle of int * int
+
+let print_shape (s : Shape) =
+    match s with
+    | Circle r -> printf "circle: %d\n" r
+    | Rectangle (width, height) -> printf "rect: %dx%d\n" width height
+
+let r1 = Rectangle(10, 20)
+let c = Circle(100)
+
+print_shape r1
+print_shape c
