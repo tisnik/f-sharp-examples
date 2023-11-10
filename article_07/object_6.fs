c8756e0b81a183932ada1029126e51bd532bbccc
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-10 09:20:54 +0100

    OOP in F#: example #6

---
+++ article_07/object_6.fs
@@ -0,0 +1,16 @@
+type Vector(x: int, y: int) =
+    member this.X = x
+    member this.Y = y
+    member this.Print() =
+        printf "Vector: %dx%d\n" this.X this.Y
+    static member (+) (a : Vector, b : Vector) =
+        Vector(a.X + b.X, a.Y + b.Y)
+
+let v1 = Vector(10, 20)
+v1.Print()
+
+let v2 = Vector(1, 2)
+v2.Print()
+
+let v3 = v1 + v2
+v3.Print()
