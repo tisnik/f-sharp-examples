cb2438f2db8d30da9b26c9864e95aa12ab1b8a3c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-10 09:20:54 +0100

    OOP in F#: example #5

---
+++ article_07/object_5.fs
@@ -0,0 +1,13 @@
+type Rectangle(x: int, y: int) =
+    member this.X = x
+    member this.Y = y
+    member this.Print() =
+        printf "Rectangle: %dx%d\n" this.X this.Y
+    member this.Enlarge(dx, dy) =
+        Rectangle(this.X + dx, this.Y + dy)
+
+let r1 = Rectangle(10, 20)
+r1.Print()
+
+let r2 = r1.Enlarge(1, 2)
+r2.Print()
