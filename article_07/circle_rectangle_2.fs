a9d23c1af31124ae35ae6edf3e5c1e79a5ea5bc7
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-13 07:55:46 +0100

    Better 'constructors'

--- article_07/circle_rectangle_2.fs
+++ article_07/circle_rectangle_2.fs
@@ -7,8 +7,8 @@ let print_shape (s : Shape) =
     | Circle r -> printf "circle: %d\n" r
     | Rectangle (width, height) -> printf "rect: %dx%d\n" width height
 
-let r1 = Rectangle(10, 20)
-let c = Circle(100)
+let r1 = Rectangle (10, 20)
+let c = Circle 100
 
 print_shape r1
 print_shape c
