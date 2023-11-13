10d178f5752c5522f2dbeaa1c55aa179507ab746
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-13 07:54:47 +0100

    Local symbol in object

---
+++ article_07/local_symbol_2.fs
@@ -0,0 +1,17 @@
+type Rectangle(x: int, y: int) =
+    let area = x * y
+    member this.X = x
+    member this.Y = y
+    member this.Print() =
+        printf "Rectangle: %dx%d %d\n" x y area
+
+let r1 = Rectangle(10, 20)
+let r2 = Rectangle(1, 2)
+
+printf "%d\n" r1.X
+printf "%d\n" r1.Y
+printf "%d\n" r1.area
+
+printf "%d\n" r2.X
+printf "%d\n" r2.Y
+printf "%d\n" r2.area
