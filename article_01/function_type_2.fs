cc71e1f87998531d1c24ea2d112b3de259e0dc2f
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-16 12:26:09 +0100

    Fixed year

--- article_01/function_type_2.fs
+++ article_01/function_type_2.fs
@@ -1,5 +1,5 @@
 (*
-    (C) Copyright 2021  Pavel Tisnovsky
+    (C) Copyright 2023  Pavel Tisnovsky
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
