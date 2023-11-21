db1f41bb745dda1c32c68fc0bd9db5fd4ea76f69
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-21 19:19:41 +0100

    Added license header

--- article_05/result_divide3.fs
+++ article_05/result_divide3.fs
@@ -1,3 +1,21 @@
+(*
+    (C) Copyright 2023  Pavel Tisnovsky
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*)
+
+
+
 let divide x y =
     match y with
     | 0 -> Error "divide by zero"
